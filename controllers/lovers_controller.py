41abd66a0af2a7b3e6e848dd2a70398e26e9e505
zhuangruiying <zhuanglinlin0321@gmail.com>
2026-01-22 00:16:51 +0800

    feat:add cors

--- controllers/lovers_controller.py
+++ controllers/lovers_controller.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, HTTPException, Depends
+import logging
+
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 import models
 import services
@@ -12,6 +14,7 @@ router = APIRouter(
 
 @router.post("/create", response_model=models.BaseResponse[models.UserLover])
 def create_lover(lover: models.UserLoverCreate, db: Session = Depends(get_db)):
+    logging.info(f"request={lover}")
     entity = services.lover_add(db, lover)
     if entity is None or entity.id < 0:
         return models.BaseResponse(
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from controllers import lovers_controller
 import uvicorn
 
@@ -8,6 +9,14 @@ app = FastAPI(
     version="1.0.0"
 )
 app.include_router(lovers_controller.router)
+# 添加 CORS 中间件
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 或指定具体前端地址，如 ["http://localhost:3000"]
+    allow_credentials=True,
+    allow_methods=["*"],  # 允许所有方法（GET, POST, OPTIONS 等）
+    allow_headers=["*"],  # 允许所有 headers
+)
 @app.get("/")
 def root():
     return {"message": "Welcome to Love API!"}
